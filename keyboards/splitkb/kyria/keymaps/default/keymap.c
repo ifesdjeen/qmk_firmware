85ea2a3ae282def226ad6ccd364f91083dc0d94e
Alex Petrov <oleksandr.petrov@gmail.com>
2023-01-10 14:04:21 +0100

    More config

--- keyboards/splitkb/kyria/keymaps/default/keymap.c
+++ keyboards/splitkb/kyria/keymaps/default/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
-     KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
+     KC_TAB, KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
      CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
      KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , FKEYS,   FKEYS,  FKEYS  ,     ADJUST,  KC_N,   KC_M ,KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
                                 ADJUST , ALT_ENT, KC_LGUI, SYM,     KC_SPC, KC_ENTER   , NAV  ,  KC_BSPC, KC_RGUI, KC_APP
@@ -83,9 +83,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_NAV] = LAYOUT(
-      _______, _______, _______, _______, _______, _______,                                     KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_VOLU, KC_DEL,
-      _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,                                     KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, KC_INS,
-      _______, _______, _______, _______, _______, _______, _______, KC_SCRL, _______, _______,KC_PAUSE, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_PSCR,
+      _______, _______, KC_EXLM, KC_AT,   KC_HASH, _______,                                     KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_VOLU, _______,
+      _______, _______, KC_DLR,  KC_PERC, KC_CIRC, _______,                                     KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, _______,
+      _______, _______, KC_AMPR, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
